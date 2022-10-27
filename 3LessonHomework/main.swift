ac47de333a8e3b93102a1a178945c201b02f05ea
Aziza <atabaevaa243@gmail.com>
2022-10-28 01:20:06 +0600

    edit

--- 3LessonHomework/main.swift
+++ 3LessonHomework/main.swift
@@ -4,42 +4,42 @@ import Foundation
 
 //    Прогноз погоды
 
-print ("Введите город, в котором хотите узнать погоду")
-var city = readLine()!
-func showWeather (_ city: String) {
-    if (city.lowercased() == "Бишкек".lowercased()) {
-        print ("В городе Бишкек сегодня +15, завтра +20, послезавтра +30")
-    }
-
-    else if (city.lowercased() == "Чолпон-ата".lowercased()) {
-        print ("В городе Чолпон-ата сегодня +10, завтра +8, послезавтра +2")
-    }
-
-    else if (city.lowercased() == "Талас".lowercased()) {
-        print ("В городе Талас сегодня +12, завтра +15, послезавтра +9")
-    }
-
-    else if (city.lowercased() == "Ош".lowercased()) {
-        print ("В городе Ош сегодня +22, завтра +18, послезавтра +16")
-    }
-
-    else if (city.lowercased() == "Джалал-Абад".lowercased()) {
-        print ("В городе Джалал-Абад сегодня +21, завтра +20, послезавтра +19")
-    }
-
-    else if (city.lowercased() == "Нарын".lowercased()) {
-        print ("В городе Нарын сегодня +9, завтра +8, послезавтра +10")
-    }
-
-    else if (city.lowercased() == "Баткен".lowercased()) {
-        print ("В городе Баткен сегодня +13, завтра +17, послезавтра +20")
-    }
-    else {
-        print("Введены неверные данные")
-    }
-}
-
-showWeather(city)
+//print ("Введите город, в котором хотите узнать погоду")
+//var city = readLine()!
+//func showWeather (_ city: String) {
+//    if (city.lowercased() == "Бишкек".lowercased()) {
+//        print ("В городе Бишкек сегодня +15, завтра +20, послезавтра +30")
+//    }
+//
+//    else if (city.lowercased() == "Чолпон-ата".lowercased()) {
+//        print ("В городе Чолпон-ата сегодня +10, завтра +8, послезавтра +2")
+//    }
+//
+//    else if (city.lowercased() == "Талас".lowercased()) {
+//        print ("В городе Талас сегодня +12, завтра +15, послезавтра +9")
+//    }
+//
+//    else if (city.lowercased() == "Ош".lowercased()) {
+//        print ("В городе Ош сегодня +22, завтра +18, послезавтра +16")
+//    }
+//
+//    else if (city.lowercased() == "Джалал-Абад".lowercased()) {
+//        print ("В городе Джалал-Абад сегодня +21, завтра +20, послезавтра +19")
+//    }
+//
+//    else if (city.lowercased() == "Нарын".lowercased()) {
+//        print ("В городе Нарын сегодня +9, завтра +8, послезавтра +10")
+//    }
+//
+//    else if (city.lowercased() == "Баткен".lowercased()) {
+//        print ("В городе Баткен сегодня +13, завтра +17, послезавтра +20")
+//    }
+//    else {
+//        print("Введены неверные данные")
+//    }
+//}
+//
+//showWeather(city)
 
 
 
@@ -93,51 +93,70 @@ showWeather(city)
 
 
 //    Доп. задание
-// func calc() {
-//    print("Введите первое число:")
-//    let num1 = readLine()!
-//    print("Какое действие хотите совершить?")
-//    let action = readLine()!
-//    print("Введите второе число:")
-//    let num2 = readLine()!
-//    if (action == "+") {
-//        func getAdd (num1: Int, num2: Int) {
-//            print("Ответ: \(num1+num2)")
-//        }
-//        getAdd(num1: Int(num1)!, num2: Int(num2)!)
-//    }
-//    else if (action == "-") {
-//        func getSub (num1: Int, num2: Int) {
-//            print("Ответ: \(num1-num2)")
-//        }
-//        getSub(num1: Int(num1)!, num2: Int(num2)!)
-//    }
-//    else if (action == "*") {
-//        func getMultiply (num1: Int, num2: Int) {
-//            print("Ответ: \(num1*num2)")
-//        }
-//        getMultiply(num1: Int(num1)!, num2: Int(num2)!)
-//    }
-//    else if (action == "/") {
-//        func getDiv (num1: Int, num2: Int) {
-//            print("Ответ: \(num1/num2)")
-//        }
-//        getDiv(num1: Int(num1)!, num2: Int(num2)!)
-//    }
-//    else {
-//        print("Введены неверные данные")
-//    }
-//    func question () {
-//        print("Продолжаем считать?")
-//        let answer = readLine()!
-//        if answer == "да" {
-//            calc()
-//        }
-//        else if answer == "нет" {
-//            print("спасибо за обращение")
-//        }
-//    }
-//    question()
-//}
-//calc()
+//print("Введите первое число:")
+//let num1 = readLine()!
+//print("Какое действие хотите совершить?")
+//let action = readLine()!
+//print("Введите второе число:")
+//let num2 = readLine()!
+
+func calc() {
+    print("Введите первое число:")
+    let num1 = readLine()!
+    print("Какое действие хотите совершить?")
+    let action = readLine()!
+    print("Введите второе число:")
+    let num2 = readLine()!
+    var ans = 0
+    
+    func getAdd (num1: Int, num2: Int) -> Int {
+        ans = num1+num2
+        return ans
+    }
+    func getSub (num1: Int, num2: Int)-> Int {
+        ans = num1-num2
+        return ans
+    }
+    func getMultiply (num1: Int, num2: Int) -> Int {
+        ans = num1*num2
+        return ans
+    }
+    func getDiv (num1: Int, num2: Int) -> Int {
+        ans = num1/num2
+        return ans
+    }
+    
+    if (action == "+") {
+        print(getAdd(num1: Int(num1)!, num2: Int(num2)!))
+    }
+     
+    else if (action == "-") {
+        print(getSub(num1: Int(num1)!, num2: Int(num2)!))
+    }
+    
+    else if (action == "*") {
+        print(getMultiply(num1: Int(num1)!, num2: Int(num2)!))
+    }
+    
+    else if (action == "/") {
+        print(getDiv(num1: Int(num1)!, num2: Int(num2)!))
+    }
+    
+    else {
+        print("Введены неверные данные")
+    }
+    
+    func question () {
+        print("Продолжаем считать?")
+        let answer = readLine()!
+        if answer == "да" {
+        calc()
+        }
+        else if answer == "нет" {
+            print("спасибо за обращение")
+        }
+    }
+    question()
+}
+calc()
 
